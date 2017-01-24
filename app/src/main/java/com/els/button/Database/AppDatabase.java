a3604f66ccff338cb46ea87b21b8bf7f2dc799d9
camdeardorff <cameron.deardorff@yahoo.com>
2017-01-24 11:25:59 -0500

    added entry point for database config

---
+++ app/src/main/java/com/els/button/Database/AppDatabase.java
@@ -0,0 +1,16 @@
+package com.els.button.Database;
+
+import com.raizlabs.android.dbflow.annotation.ConflictAction;
+import com.raizlabs.android.dbflow.annotation.Database;
+
+/**
+ * Created by Cam on 1/17/17.
+ */
+
+@Database(name = AppDatabase.NAME, version = AppDatabase.VERSION, foreignKeyConstraintsEnforced = true, insertConflict = ConflictAction.IGNORE, updateConflict= ConflictAction.REPLACE)
+public class AppDatabase {
+
+    public static final String NAME = "AppDatabase"; // we will add the .db extension
+
+    public static final int VERSION = 4;
+}
