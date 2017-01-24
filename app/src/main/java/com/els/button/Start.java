e6ccce8afe51d8b4c4c95469f6b95e6caa0bdc2c
camdeardorff <cameron.deardorff@yahoo.com>
2017-01-24 11:27:20 -0500

    added entry point for app to start database

---
+++ app/src/main/java/com/els/button/Start.java
@@ -0,0 +1,23 @@
+package com.els.button;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+/**
+ * Created by Cam on 1/17/17.
+ */
+
+public class Start extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // start DBFlow instance now
+        FlowManager.init(new FlowConfig.Builder(this).openDatabasesOnInit(true).build());
+
+        Log.d("START", "BAM!: on create in Start");
+    }
+}
