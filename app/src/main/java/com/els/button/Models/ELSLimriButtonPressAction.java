c44857eb0a849f287ed93827c264a9602cc30b5b
camdeardorff <cameron.deardorff@yahoo.com>
2017-03-23 14:08:53 -0400

    Added more cases

--- app/src/main/java/com/els/button/Models/ELSLimriButtonPressAction.java
+++ app/src/main/java/com/els/button/Models/ELSLimriButtonPressAction.java
@@ -5,8 +5,11 @@ package com.els.button.Models;
  */
 
 public enum ELSLimriButtonPressAction {
-    LOADSHEET("loadSheet"),
-    LOADURL("loadURL"),
+    SET_QUESTION("set-question"),
+    INC_QUESTION("inc-question"),
+    LOAD_SHEET("load-sheet"),
+    LOAD_URL("load-url"),
+    REFRESH("refresh"),
     NOTHING("nothing");
 
     private String literalAction;
@@ -20,12 +23,12 @@ public enum ELSLimriButtonPressAction {
     }
 
     static public ELSLimriButtonPressAction fromStringLiteral(String stringLiteral) {
-        if (stringLiteral.equals("loadSheet")) {
-            return LOADSHEET;
-        } else if (stringLiteral.equals("loadURL")) {
-            return LOADURL;
-        } else {
-            return NOTHING;
-        }
+
+       if (SET_QUESTION.getLiteralAction().equals(stringLiteral)) {         return SET_QUESTION; }
+       else if (INC_QUESTION.getLiteralAction().equals(stringLiteral)) {    return INC_QUESTION; }
+       else if (LOAD_SHEET.getLiteralAction().equals(stringLiteral)) {      return LOAD_SHEET; }
+       else if (LOAD_URL.getLiteralAction().equals(stringLiteral)) {        return LOAD_URL; }
+       else if (REFRESH.getLiteralAction().equals(stringLiteral)) {         return REFRESH; }
+       else {                   /* nothing */                               return NOTHING; }
     }
 }
