bfdd65e9b74c306940157280912eef4f7ba269aa
camdeardorff <cameron.deardorff@yahoo.com>
2017-04-06 14:59:09 -0400

    moved to interfaces folder

--- app/src/main/java/com/els/button/Interfaces/InventoryListAdapterDelegate.java
+++ app/src/main/java/com/els/button/Interfaces/InventoryListAdapterDelegate.java
@@ -1,4 +1,8 @@
-package com.els.button.Models;
+package com.els.button.Interfaces;
+
+import com.els.button.Models.ELSIoT;
+import com.els.button.Models.ELSLimri;
+import com.els.button.Models.ELSLimriButtonPressAction;
 
 /**
  * Created by cameron on 3/31/16.
@@ -6,5 +10,6 @@ package com.els.button.Models;
 public interface InventoryListAdapterDelegate {
 
     public void limriButtonWasPressed(ELSLimri elsLimri, ELSLimriButtonPressAction action);
+    public void limriConfigurationButtonWasPressed(ELSLimri elsLimri);
     public void iotButtonWasPressed(ELSIoT elsIoT, String value);
 }
